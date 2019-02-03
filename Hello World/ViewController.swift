9a56b72e130d1d4c0839911f24aeb4e08d6b0a74
Brian Hillis <brian@ATM0560395DYN.dhcp.missouri.edu>
2019-02-03 15:56:20 -0600

    First commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Brian Hillis on 2/3/19.
+//  Copyright © 2019 Brian Hillis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
